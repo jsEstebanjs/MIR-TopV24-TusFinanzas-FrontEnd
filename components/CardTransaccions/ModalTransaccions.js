877e105ee7bc70da6dd8329a4e3a56700d688083
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-11-04 16:35:09 -0500

    add card transaccions home

--- components/CardTransaccions/ModalTransaccions.js
+++ components/CardTransaccions/ModalTransaccions.js
@@ -1,23 +1,34 @@
-import styles from './ModalTransaccions.module.scss';
-import Image from 'next/image';
+import styles from "./ModalTransaccions.module.scss";
+import Image from "next/image";
 
-function ModalTransaccions({amount,date,category,wallet}){
-    return(
+function ModalTransaccions({ amount, date, category, wallet, type , img }) {
+  return (
+    <div
+      className={`${styles.mainContainerModalTransaccions} ${
+        type === "Entry"
+          ? styles.mainContainerInfoModalTransaccionsBorderGreen
+          : styles.mainContainerInfoModalTransaccionsBorderRed
+      }`}
+    >
+      <Image
+        height={50}
+        width={50}
+        src={img}
+      />
+      <div className={`${styles.containerInfoModalTransaccions}`}>
         <div>
-            <Image
-            height={100}
-            />
-            <div >
-                <div>
-                    <p>{category}</p>
-                    <p>{wallet}</p>
-                </div>
-                <div>
-                    <p>{amount}</p>
-                    <p>{date}</p>
-                </div>
-            </div>
+          <p>{category}</p>
+          <p className={styles.infoPWallet}>{wallet}</p>
         </div>
-    )
+        <div>
+          <p className={styles.infoPAmount}>
+            ${type === "Expense" ? "-" : null}
+            {amount}
+          </p>
+          <p>{date}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default ModalTransaccions;
--- components/CardTransaccions/index.js
+++ components/CardTransaccions/index.js
@@ -1,12 +1,41 @@
-import styles from './index.module.scss';
-import ModalTransaccions from './ModalTransaccions';
+import styles from "./index.module.scss";
+import ModalTransaccions from "./ModalTransaccions";
 
-function CardTransaccions(){
-    return(
-        <div className={styles.mainContainerCardTransaccions}>
-            <h2 className={styles.titleContainerCardTransaccions}>Transacciones</h2>
-            <ModalTransaccions />
-        </div>
-    )
+function CardTransaccions() {
+  return (
+    <div className={styles.mainContainerCardTransaccions}>
+      <h2 className={styles.titleContainerCardTransaccions}>Transacciones</h2>
+      <div className={styles.containerModalTransaccions}>
+        <ModalTransaccions
+          amount={40000}
+          wallet={"Cartera"}
+          category={"Salario"}
+          date={"11/04/2022"}
+          type={"Expense"}
+          img={
+            "https://res.cloudinary.com/dvdun5uli/image/upload/v1667524253/category%20default%20images/Spents/Bares_zl91th.png"
+          }
+        />
+        <ModalTransaccions
+          amount={40000}
+          wallet={"Cartera"}
+          category={"Salario"}
+          date={"11/04/2022"}
+          type={"Entry"}
+          img={
+            "https://res.cloudinary.com/dvdun5uli/image/upload/v1667524041/category%20default%20images/Spents/Tecnologia_dwfusu.png"
+          }
+        />
+        <ModalTransaccions
+          amount={40000}
+          wallet={"Cartera"}
+          category={"Salario"}
+          date={"11/04/2022"}
+          type={"Expense"}
+          img={"https://res.cloudinary.com/dvdun5uli/image/upload/v1667524042/category%20default%20images/Spents/Transportes_jzkznl.png"}
+        />
+      </div>
+    </div>
+  );
 }
 export default CardTransaccions;
