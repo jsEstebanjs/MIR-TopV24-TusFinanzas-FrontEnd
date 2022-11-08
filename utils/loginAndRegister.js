a77bd35c1cd8adcc0275aee8b5a800a56664992a
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-11-08 09:27:38 -0500

    add env

--- utils/loginAndRegister.js
+++ utils/loginAndRegister.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const loginAndRegister = async(url,info) => {
     try{
-        const res = await axios.post(url,{
+        const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/users/${url}`,{
         ...info
         })
         return res
