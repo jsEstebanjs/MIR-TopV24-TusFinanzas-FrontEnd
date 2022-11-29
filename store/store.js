7745af42a27f07cc31160b923b636c56519f5c61
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-11-28 23:23:28 -0500

    fix store log out two

--- store/store.js
+++ store/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userSlice from './user.Slice'
 import transaccionsSlice from './transaccions.Slice';
-import logOutSlice from './logOut.slice';
 
 export const store = configureStore({
   reducer: {
